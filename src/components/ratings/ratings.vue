4858aecce4dd75c272096975866c726ee1811f64
hj1226104386 <1226104386@qq.com>
2017-09-21 19:02:00 +0800

    评论页的评论列表部分

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -25,12 +25,22 @@
         </div>
       </div>
     </div>
+    <div class="ratingList whiteBg">
+        <ul class="selectRatingType">
+          <li>全部<span>57</span></li>
+          <li>满意<span>47</span></li>
+          <li>不满意<span>10</span></li>
+        </ul>
+      <div class="ratingsWithContent">
+        <i></i>
+        <span>只看有内容的评价</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import star from '../star/star.vue'
-
   export default {
     name: 'ratings',
     props: ['headerData'],
@@ -58,7 +68,6 @@
     border-bottom: 1px solid rgba(7, 17, 27, .1);
     margin-bottom: 16px;
   }
-
   #ratings {
     width: 100%;
     background-color: rgba(7, 17, 27, .1);
@@ -66,26 +75,23 @@
     top: 174px;
     bottom: 46px;
   }
-
   .shopRating {
     width: 100%;
     padding: 18px 0;
     display: flex;
+    margin-bottom: 18px;
   }
-
   .ratingLeft {
     width: 36.7%;
     text-align: center;
     border-right: 1px solid rgba(7, 17, 27, .1);
     flex: 0 0 36.7%;
   }
-
   .ratingLeft > h4 {
     font-size: 24px;
     color: rgb(255, 153, 0);
     line-height: 28px;
   }
-
   .ratingLeft > span {
     font-size: 12px;
     color: rgb(7, 17, 27);
@@ -93,41 +99,34 @@
     display: inline-block;
     margin: 6px 0 8px;
   }
-
   .ratingLeft > p {
     font-size: 10px;
     color: rgba(7, 17, 27, .5);
   }
-
   .ratingRight {
     text-align: center;
     padding: 0 24px;
     flex: 1;
     font-size: 0;
   }
-
   .ratingWrapper {
     display: inline-block;
     height: 100%;
     width: 190px;
   }
-
   .ratingWrapper > div {
     text-align: left;
   }
-
   .foodRate,
   .serviceRate {
     margin-bottom: 8px;
   }
-
   .rateTitle {
     font-size: 12px;
     color: rgb(7, 17, 27);
     line-height: 18px;
     margin-right: 12px;
   }
-
   .expressTime > i {
     font-style: normal;
     font-size: 12px;
@@ -135,7 +134,6 @@
     line-height: 18px;
     color: rgb(147, 153, 159);
   }
-
   .foodRate > i,
   .serviceRate > i {
     font-style: normal;
@@ -143,5 +141,9 @@
     color: rgb(255, 153, 0);
     line-height: 18px;
     margin-left: 12px;
+  }
+  .ratingList{
+    border-bottom-width:0;
+    padding:18px;
   }
 </style>
